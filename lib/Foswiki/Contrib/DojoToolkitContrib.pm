1a7d8f7cf095ac924022086251cdfdb77c9e280f
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 12:53:56 +0000

    Item11808: revert perltidy on all lib/CPAN and all plugins which are CoordinateWithAuthor, as requested by Micha
    git-svn-id: http://svn.foswiki.org/trunk/DojoToolkitContrib@14764 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/DojoToolkitContrib.pm
+++ lib/Foswiki/Contrib/DojoToolkitContrib.pm
@@ -1,10 +1,11 @@
 package Foswiki::Contrib::DojoToolkitContrib;
 use vars qw( $VERSION $pluginName );
-$VERSION    = '$Rev$';
+$VERSION = '$Rev$';
 $pluginName = 'DojoToolkitContrib';
 
 use strict;
 use Foswiki::Func;
+
 
 #not sure its going to be useful, but....
 #this would allow multiple contribs/plugins to require the same components,
@@ -13,15 +14,14 @@ use Foswiki::Func;
 #   and that will have to be done in a Plugin, to hook into the completePageHandler
 sub requireJS {
     my $module = shift;
-
+    
     #TODO: ( $name, $skin, $web ) paramters?
     my $javascript = Foswiki::Func::loadTemplate('javascript.dojo');
-    Foswiki::Func::addToHEAD( 'AA' . $pluginName . '.javascript.dojo',
-        $javascript );
-
-    my $output =
-      "<script type=\"text/javascript\">dojo.require(\"$module\");</script>";
-    Foswiki::Func::addToHEAD( 'AB' . $pluginName . '.' . $module, $output );
+    Foswiki::Func::addToHEAD('AA'.$pluginName.'.javascript.dojo', $javascript);
+    
+    my $output = "<script type=\"text/javascript\">dojo.require(\"$module\");</script>";
+    Foswiki::Func::addToHEAD('AB'.$pluginName.'.'.$module, $output);
 }
+
 
 1;
--- lib/Foswiki/Contrib/DojoToolkitContrib/build.pl
+++ lib/Foswiki/Contrib/DojoToolkitContrib/build.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
 use strict;
-
 # Standard preamble
 BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ) }
 
@@ -13,7 +12,7 @@ our @ISA = qw( Foswiki::Contrib::Build );
 
 sub new {
     my $class = shift;
-    return bless( $class->SUPER::new("DojoToolkitContrib"), $class );
+    return bless( $class->SUPER::new( "DojoToolkitContrib" ), $class );
 }
 
 # Example: Override the build target
@@ -29,5 +28,5 @@ sub target_build {
 my $build = new BuildBuild();
 
 # Build the target on the command line, or the default target
-$build->build( $build->{target} );
+$build->build($build->{target});
 
